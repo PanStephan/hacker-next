c321596062639e0895f7dbcc49bc1f174210f98d
Stephan <stephan.startsev@gmail.com>
2020-08-02 20:05:32 +0300

    finish the app

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,43 @@
-const Index = () => (
-  <div>Hacker next</div>
-)
+import fetch from 'isomorphic-fetch'
+import Error from 'next/error'
+import StoryList from '../components/StoryList'
+import Link from 'next/link'
+
+const Index = ({ stories, page }) => {
+
+  if (stories.length === 0) return <Error statusCode={503}/>
+
+  return (
+    <div>
+      <div>Hacker next</div>
+      <StoryList stories={stories}/> 
+      <footer>
+        <Link href={`/?page=${page+1}`}>
+          <a>Next Page {page+1}</a>
+        </Link>
+      </footer>
+    </div>
+  )
+}
+
+export async function getServerSideProps({ req, res, query }) {
+  let stories
+  let page 
+  try {
+    page = Number(query.page) || 1
+    const res = await fetch(`https://node-hnapi.herokuapp.com/news?page=${page}`)
+    stories = await res.json()
+  } catch(e) { 
+    console.log(e) 
+    stories = []
+  }
+  return {
+    props: {
+      stories,
+      page
+    }
+  }
+}
+
 
 export default Index
